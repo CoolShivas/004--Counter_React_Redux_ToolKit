992dc629cef65c9f191956501219a35e8b2fefe7
CoolShivas <shivas2710cool00@gmail.com>
2024-05-22 23:51:04 +0530

    Implementation of ReduxToolkit (i.e, configureStore) with Provider store

--- counter_react_redux_toolkit/src/main.jsx
+++ counter_react_redux_toolkit/src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import counterStore from './store/index.js';
+import counterConfigStore from './store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={counterStore}>
+    <Provider store={counterConfigStore}>
       <App />
     </Provider>
   </React.StrictMode>,
--- counter_react_redux_toolkit/src/store/index.js
+++ counter_react_redux_toolkit/src/store/index.js
@@ -1,49 +1,59 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const INITIAL_VALUE = {
-  counter: 0,
-  privacy: false,
-};
+// const INITIAL_VALUE = {
+//   counter: 0,
+//   privacy: false,
+// };
 
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  //console.log("Action Received", action);
-  // // Now, if you click on +1 we will get the output as Action Received {type: 'INCREMENT'};
-  // // Now, if you click on -1 we will get the output as Action Received {type: 'DECREMENT'};
-  if (action.type === "INCREMENT") {
-    return {
-      ...store, // It will print the previous key and value;
-      counter: store.counter + 1,
-      // privacy: store.privacy,// Now, with the help of spread operator there is no need of mentioning privacy or counter if spread operatore used with store;
-    };
-  } else if (action.type === "DECREMENT") {
-    return {
-      ...store,
-      counter: store.counter - 1,
-      // privacy: store.privacy,
-    };
-  } else if (action.type === "ADDITION") {
-    return {
-      ...store,
-      counter: store.counter + action.payload.num,
-      // privacy: store.privacy,
-    };
-  } else if (action.type === "SUBTRACTION") {
-    return {
-      ...store,
-      counter: store.counter - action.payload.num,
-      // privacy: store.privacy,
-    };
-  } else if (action.type === "PRIVACY_TOGGLE") {
-    return {
-      ...store,
-      // counter: store.counter, // And Counter value is remain same here as it is;
-      privacy: !store.privacy, // Now privacy is true ;
-    };
-  } else {
-    return store;
-  }
-};
+// const counterReducer = (store = INITIAL_VALUE, action) => {
+//   //console.log("Action Received", action);
+//   // // Now, if you click on +1 we will get the output as Action Received {type: 'INCREMENT'};
+//   // // Now, if you click on -1 we will get the output as Action Received {type: 'DECREMENT'};
+//   if (action.type === "INCREMENT") {
+//     return {
+//       ...store, // It will print the previous key and value;
+//       counter: store.counter + 1,
+//       // privacy: store.privacy,// Now, with the help of spread operator there is no need of mentioning privacy or counter if spread operatore used with store;
+//     };
+//   } else if (action.type === "DECREMENT") {
+//     return {
+//       ...store,
+//       counter: store.counter - 1,
+//       // privacy: store.privacy,
+//     };
+//   } else if (action.type === "ADDITION") {
+//     return {
+//       ...store,
+//       counter: store.counter + action.payload.num,
+//       // privacy: store.privacy,
+//     };
+//   } else if (action.type === "SUBTRACTION") {
+//     return {
+//       ...store,
+//       counter: store.counter - action.payload.num,
+//       // privacy: store.privacy,
+//     };
+//   } else if (action.type === "PRIVACY_TOGGLE") {
+//     return {
+//       ...store,
+//       // counter: store.counter, // And Counter value is remain same here as it is;
+//       privacy: !store.privacy, // Now privacy is true ;
+//     };
+//   } else {
+//     return store;
+//   }
+// };
 
-const counterStore = createStore(counterReducer);
+// const counterStore = createStore(counterReducer);
 
-export default counterStore;
+// export default counterStore;
+
+//////************************************************************************************************************* */
+
+// // Now, going to use REDUX TOOLKIT whereas the above one is formed using redux and react-redux :-
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const counterConfigStore = configureStore({});
+
+export default counterConfigStore;
