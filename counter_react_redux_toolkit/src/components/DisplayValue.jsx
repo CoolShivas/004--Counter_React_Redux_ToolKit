e6e4e58e0db2cdd46fc4a6955de261666aa5821d
CoolShivas <shivas2710cool00@gmail.com>
2024-05-23 12:37:11 +0530

    Grabbing the slice of counter using useSelector

--- counter_react_redux_toolkit/src/components/DisplayValue.jsx
+++ counter_react_redux_toolkit/src/components/DisplayValue.jsx
@@ -2,13 +2,23 @@ import { useSelector } from "react-redux";
 
 const DisplayValue = () => {
 
-    const counter = useSelector((store) => store.counter);
+    // const counter = useSelector((store) => store.counter);
     // With the help of useSelector we are now able to access the store folder's INITIAL_VALUE i.e, 5 ;
+
+    // Now, using reduxToolKit :-
+
+    // const counterObj = useSelector((store) => store.counter);
+    // const counter = counterObj.counterValue;
+
+    // Or we can destructure the above counterValue directly :-
+
+    const { counterValue } = useSelector((store) => store.counter)
 
     return (
         // <p className="lead mb-4"> Counter Value is = 0 .</p>
-        <p className="lead mb-4"> Counter Value is =  {counter} .</p>
+        // <p className="lead mb-4"> Counter Value is =  {counter} .</p>
         // Here, we will get the Counter Value is = 5 ;
+        <p className="lead mb-4"> Counter Value is =  {counterValue} .</p>
     )
 }
 
